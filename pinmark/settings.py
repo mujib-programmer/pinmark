56d8a86e7b6c414a20707c2e81ba50e4f61971ef
PHPGeek Programmer <mujib.programmer@gmail.com>
2015-05-28 15:22:43 +0700

    Using sqlite3 as database backend.

--- pinmark/settings.py
+++ pinmark/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'pinmark.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'pinmark.db.sqlite3'),
     }
 }
 
